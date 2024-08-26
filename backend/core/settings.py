e6f4d540d4bfbdc5682b99a52c9beada25907155
dannycod3r <oseid7807@gmail.com>
2024-08-26 21:33:41 +0000

    setting for django-cors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'drf_spectacular_sidecar',  # required for Django collectstatic discovery
     'api',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -60,6 +61,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -70,6 +72,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "core.urls"
+
+# Allow all domains to access your Django API
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
